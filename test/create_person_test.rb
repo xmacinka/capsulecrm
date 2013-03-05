600592ccbefc0df7466b28c4eb12558619738600
James Smith <james@floppy.org.uk>
2013-03-05 13:08:44 +0000

    Test serialisation of person contacts

--- test/create_person_test.rb
+++ test/create_person_test.rb
@@ -10,6 +10,10 @@ class CreatePersonTest < Test::Unit::TestCase
     person = CapsuleCRM::Person.new
     person.first_name = 'Homer'
     person.last_name = 'Simpson'
+    person.emails << CapsuleCRM::Email.new(person, :type => 'Work', :address => 'homer.simpson@springfield-nuclear.com')
+    person.phone_numbers << CapsuleCRM::Phone.new(person, :type => 'Work', :number => '+1 888 555555')
+    person.websites << CapsuleCRM::Website.new(person, :type => 'Work', :web_address => 'www.springfield-nuclear.com', :web_service => 'URL')
+    person.addresses << CapsuleCRM::Address.new(person, :type => 'Office', :street => '100 Industrial Way', :city => 'Springfield', :state => "??", :zip => '12345', :country => 'United States')
     # test that XML is generated correctly
     xml = CapsuleCRM::Person.attributes_to_xml(person.attributes)
     assert_equal xml, <<-EOF
@@ -21,6 +25,29 @@ class CreatePersonTest < Test::Unit::TestCase
   <title nil="true"/>
   <jobTitle nil="true"/>
   <organisationId nil="true"/>
+  <contacts>
+    <email>
+      <type>Work</type>
+      <emailAddress>homer.simpson@springfield-nuclear.com</emailAddress>
+    </email>
+    <phone>
+      <type>Work</type>
+      <phoneNumber>+1 888 555555</phoneNumber>
+    </phone>
+    <website>
+      <type>Work</type>
+      <webAddress>www.springfield-nuclear.com</webAddress>
+      <webService>URL</webService>
+    </website>
+    <address>
+      <type>Office</type>
+      <street>100 Industrial Way</street>
+      <city>Springfield</city>
+      <state>??</state>
+      <zip>12345</zip>
+      <country>United States</country>
+    </address>
+  </contacts>
 </person>
     EOF
   end
