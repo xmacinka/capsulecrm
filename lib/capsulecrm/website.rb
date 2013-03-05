8cac4ab51906bbeb826fe88c3783ce33589c23f4
James Smith <james@floppy.org.uk>
2013-03-05 13:04:40 +0000

    Make Website inherit from Contact and remove url

--- lib/capsulecrm/website.rb
+++ lib/capsulecrm/website.rb
@@ -1,13 +1,12 @@
-class CapsuleCRM::Website < CapsuleCRM::Child
+class CapsuleCRM::Website < CapsuleCRM::Contact
 
   attr_accessor :web_address
-  attr_accessor :url
   attr_accessor :web_service
 
   # nodoc
   def attributes
     attrs = {}
-    arr = [:type, :web_address, :url, :web_service]
+    arr = [:type, :web_address, :web_service]
     arr.each do |key|
       attrs[key] = self.send(key)
     end
@@ -18,7 +17,6 @@ class CapsuleCRM::Website < CapsuleCRM::Child
   def self.xml_map
     map = {
       'webAddress' => 'web_address',
-      'url' => 'url',
       'webService' => 'web_service'
     }
     super.merge map
