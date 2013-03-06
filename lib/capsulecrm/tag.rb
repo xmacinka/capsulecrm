78a206b1258d22a7a180b69209a2acc8a7c2ab4a
James Smith <james@floppy.org.uk>
2013-03-06 21:10:01 +0000

    Add ability to create tags

--- lib/capsulecrm/tag.rb
+++ lib/capsulecrm/tag.rb
@@ -2,6 +2,25 @@ class CapsuleCRM::Tag < CapsuleCRM::Child
 
   attr_accessor :name
 
+  def attributes
+    attrs = {}
+    arr = [:name]
+    arr.each do |key|
+      attrs[key] = self.send(key)
+    end
+    attrs
+  end
+
+  # nodoc
+  def save
+    create
+  end
+
+  # nodoc
+  def self.xml_root
+    'tag'
+  end
+
   # nodoc
   def self.xml_map
     map = {
@@ -9,4 +28,22 @@ class CapsuleCRM::Tag < CapsuleCRM::Child
     }
     super.merge map
   end
+
+  private
+
+  # nodoc
+  def create
+    path = parent.class.get_path+'/'+parent.id.to_s+'/tag/'+name
+    options = {:path => path, :wrap => 'tags'}
+    new_id = self.class.create attributes, options
+    unless new_id
+      errors << self.class.last_response.response.message
+      return false
+    end
+    @errors = []
+    changed_attributes.clear
+    self.id = new_id
+    self
+  end
+
 end
